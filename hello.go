bd81a941d9c6f3b59d984829434846fc74f19e8d
ZhiyuSun <zcgyxlgy@sina.com>
2021-05-01 15:22:25 +0800

    first commit

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. 代码第一行package main
+	// main函数，将主要逻辑放到main函数中
+	println("Hello world")
+	print("hello")
+	print("world")
+
+	// 尽量用这种打印
+	fmt.Println("hello world")
+	fmt.Println("golang")
+}
